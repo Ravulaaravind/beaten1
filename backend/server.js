d161a0ea8116d77b6a1244550b8d46ba64247e61
Ravulaaravind <130533726+Ravulaaravind@users.noreply.github.com>
2025-07-13 20:31:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,10 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
